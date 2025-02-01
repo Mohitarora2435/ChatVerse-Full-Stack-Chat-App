bf919a9734470c21ea26af6665a5916dd355d753
Mohit Arora <92647348+Mohitarora2435@users.noreply.github.com>
2025-02-01 17:08:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
